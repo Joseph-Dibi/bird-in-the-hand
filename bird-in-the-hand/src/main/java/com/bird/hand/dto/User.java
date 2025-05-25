c8f4f95914ebd2a652366d9b55f767e1360dde59
Joseph DiBiasi <joseph.dibiasi@du.edu>
2025-05-25 17:58:57 -0600

    manually adding getter setters so no need of lombok

--- bird-in-the-hand/src/main/java/com/bird/hand/dto/User.java
+++ bird-in-the-hand/src/main/java/com/bird/hand/dto/User.java
@@ -4,14 +4,52 @@ import java.util.Date;
 
 import lombok.Data;
 
-@Data
+
 public class User {
 	
-	String username;
-	String profilePicture;
-	String photos;
-	String password;
-	String email;
-	Date memberSince;
+	private String username;
+	private String profilePicture;
+	private String photos;
+	private String password;
+	private String email;
+	private Date memberSince;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+	public String getPhotos() {
+		return photos;
+	}
+	public void setPhotos(String photos) {
+		this.photos = photos;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getMemberSince() {
+		return memberSince;
+	}
+	public void setMemberSince(Date memberSince) {
+		this.memberSince = memberSince;
+	}
+
 
 }
