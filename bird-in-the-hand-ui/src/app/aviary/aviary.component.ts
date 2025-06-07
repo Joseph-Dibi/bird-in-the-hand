09a74e1716828c9cb02addeb2870613caffb0f36
Joseph DiBiasi <joseph.dibiasi@du.edu>
2025-06-07 13:53:15 -0600

    Aviary Tweaks

--- bird-in-the-hand-ui/src/app/aviary/aviary.component.ts
+++ bird-in-the-hand-ui/src/app/aviary/aviary.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AviaryPhoto } from '../models/models';
 import { MatTableDataSource } from '@angular/material/table';
 import { BirdManagementService } from '../bird-management.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DetailViewComponent } from '../detail-view/detail-view.component';
 
 @Component({
   selector: 'app-aviary',
@@ -10,14 +12,14 @@ import { BirdManagementService } from '../bird-management.service';
   styleUrl: './aviary.component.scss',
 })
 export class AviaryComponent implements OnInit {
-
   dataSource = new MatTableDataSource<AviaryPhoto>();
   displayedColumns: string[] = ['photo'];
 
-  constructor(private birdManagementService: BirdManagementService) {
+  constructor(
+    private birdManagementService: BirdManagementService,
+    private dialog: MatDialog
+  ) {}
 
-
-  }
   ngOnInit(): void {
     /*
       The data source is set to the aviaryPhotoList array, which contains the list of photos.
@@ -26,8 +28,18 @@ export class AviaryComponent implements OnInit {
       the data source will be set to the result of the API call to fetch the photos. There may be a later need to override
       the default features of the MatTableDataSource to add custom sorting or filtering logic.
     */
-    this.birdManagementService.getAviaryPhotos().subscribe((photos: AviaryPhoto[]) => {
-      this.dataSource = new MatTableDataSource(photos);
+    this.birdManagementService
+      .getAviaryPhotos()
+      .subscribe((photos: AviaryPhoto[]) => {
+        this.dataSource = new MatTableDataSource(photos);
+      });
+  }
+
+  openPhotoDetailDialog(aviaryPhoto: AviaryPhoto) {
+    this.dialog.open(DetailViewComponent, {
+      data: aviaryPhoto,
     });
   }
+
+  voteForPhoto(aviaryPhoto: AviaryPhoto) {}
 }
